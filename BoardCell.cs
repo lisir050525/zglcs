ae9b557dea81fdb78ce44829b181b968b4643b20
DESKTOP-467RVN8\lisir <3786394770@qq. com>
2025-05-06 10:09:46 +0800

    五子棋

---
+++ BoardCell.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BoardCell : MonoBehaviour
+{
+    private int x, y;
+
+    public void Initialize(int xPos, int yPos)
+    {
+        x = xPos;
+        y = yPos;
+        GetComponent<Button>().onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        GameManager.Instance.PlaceChess(x, y);
+    }
+}
